dd23fefb9f1108cce1e1db116a4a4be6985d8a62
vuoriov <vuoriov4@gmail.com>
2017-08-28 22:12:56 +0300

    Updates

--- client.js
+++ client.js
@@ -1,33 +1,17 @@
 const btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
-const TARGET_ADDRESS = "60:6B:BD:10:8A:59"; // Replace with bluetooth address of your target device.
+const TARGET_ADDRESS = "B8:27:EB:D1:4E:7A"; // Replace with bluetooth address of your target device.
 const TARGET_CHANNEL = 1;
-
-console.log("Scanning for bluetooth devices...");
-btSerial.on('found', function(address, name) {
-    console.log("Found a device (" + address + ")");
-    if (address === TARGET_ADDRESS) {
-        console.log("Target device detected! Connecting to channel " + TARGET_CHANNEL + "...");
-        btSerial.findSerialPortChannel(address, function(channel) {
-            if (channel === TARGET_CHANNEL) {
-                btSerial.connect(address, channel, function() {
-                    console.log("Success! Sending message...");
-                    btSerial.write(new Buffer('Hello world!', 'utf-8'), function(err, bytesWritten) {
-                        if (err) console.log(err);
-                    });
-                    btSerial.on('data', function(buffer) {
-                        console.log("Response: " + buffer.toString('utf-8'));
-                    });
-                }, function() {
-                    console.log("Error: Can't connect to channel " + TARGET_CHANNEL + ".");
-                });
-                btSerial.close();
-            }
-    	}, function() {
-    		console.log('Error: No channels found');
-    	});
-    } else {
-        console.log("That's not the target device.");
-    }
+console.log('Connecting to ' + TARGET_ADDRESS + "...");
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+btSerial.connect(TARGET_ADDRESS, TARGET_CHANNEL, function() {
+    console.log("Connected!");
+    btSerial.write(new Buffer("Hello world!", 'utf-8'), function(err, count) {
+        console.error(err);
+    });
+    btSerial.on('data', function(data) {
+        console.log(data.toString('utf-8'));
+    });
+}, function (err) {
+    console.error(err);
 });
-
-btSerial.inquire();
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const server = new(require('bluetooth-serial-port')).BluetoothSerialPortServer();
 const TARGET_CHANNEL = 1;
 const TARGET_UUID = '7a379a32-63ce-449d-b71a-40f4e6616458';
-console.log("Listening for bluetooth at channel " + TARGET_CHANNEL + "...");
+console.log("Listening for bluetooth connections...");
 server.listen(function (clientAddress) {
     console.log('Client ' + clientAddress + ' connected.');
     server.on('data', function(buffer) {
-        console.log('Received data from client: ' + buffer);
-        server.write(new Buffer('Hello there!'));
+        console.log('Received message: "' + buffer + '"');
+        server.write(new Buffer('Thanks for the message!'), function(error) {
+            console.error(error)
+        });
     });
 }, function(error){
 	console.error(error);
